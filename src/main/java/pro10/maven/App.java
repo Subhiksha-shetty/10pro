33b66a0e8fb09093e8a0d3d14a7ccf55c64e96e2
Subhiksha Shetty <shettysubhiksha9@gmail.com>
2025-06-03 13:26:47 +0530

    Initial commit

---
+++ src/main/java/pro10/maven/App.java
@@ -0,0 +1,13 @@
+package pro10.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
